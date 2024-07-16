a0ed6de0bb9b711942a3f78740407a5603a1ef61
Opp Lieam <runicer156@gmail.com>
2024-07-17 01:40:10 +0700

    feat(webservice): add basic http server and slog

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger = logger.With("service", "bb-admin-api")
+
+	if err := run(logger); err != nil {
+		logger.Error("failed to start server", "error", err)
+	}
+
+}
+
+func run(log *slog.Logger) error {
+
+	log.Info("start up", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	r := gin.Default()
+
+	r.Use(gin.Recovery())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
