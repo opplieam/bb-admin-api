6f3cd8abf904cbd44692e5670c5fe14c15a03788
Opp Lieam <runicer156@gmail.com>
2024-07-18 02:42:24 +0700

    feat(webservice): add grateful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/opplieam/bb-admin-api/internal/middleware"
@@ -16,7 +22,7 @@ func main() {
 	logger = logger.With("service", "bb-admin-api", "build", build)
 
 	if err := run(logger); err != nil {
-		logger.Error("failed to start server", "error", err)
+		logger.Error("start up", "error", err)
 	}
 
 }
@@ -41,9 +47,37 @@ func run(log *slog.Logger) error {
 		})
 	})
 
-	err := r.Run(":3000")
-	if err != nil {
-		return err
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         ":3000",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      r.Handler(),
+	}
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Info("startup", "status", "api router started", "address", srv.Addr)
+		serverErrors <- srv.ListenAndServe()
+	}()
+
+	// Shutdown
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+	case sig := <-shutdown:
+		log.Info("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Info("shutdown", "status", "shutdown complete", "signal", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			_ = srv.Close()
+			return fmt.Errorf("could not shutdown gratefuly: %w", err)
+		}
 	}
 
 	return nil
